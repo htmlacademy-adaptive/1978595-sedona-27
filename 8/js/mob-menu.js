5dee5ee68e9e2150b9c69ffffb25d8fb32f059da
Keks <keks@htmlacademy.ru>
2023-01-14 02:42:43 +0300

    :heavy_check_mark: Сборка #8

---
+++ 8/js/mob-menu.js
@@ -0,0 +1,20 @@
+let menubutton = document.querySelector('.main-navigation__toggle');
+let navmenu = document.querySelector('.main-navigation__navigation-menu');
+let iconBurger = document.querySelector('.menu-toggle__burger');
+let iconCross = document.querySelector('.menu-toggle__cross');
+
+navmenu.classList.remove('main-navigation__navigation-menu--no-js')
+
+function toggleMenu() {
+  if (navmenu.classList.contains('main-navigation__navigation-menu--open') === true) {
+    navmenu.classList.remove('main-navigation__navigation-menu--open');
+    iconBurger.classList.remove('menu-toggle__burger--invisible');
+    iconCross.classList.remove('menu-toggle__cross--visible');
+  }
+  else {
+    navmenu.classList.add('main-navigation__navigation-menu--open');
+    iconBurger.classList.add('menu-toggle__burger--invisible');
+    iconCross.classList.add('menu-toggle__cross--visible');
+  }
+}
+menubutton.addEventListener('click', toggleMenu);
