a2288ae93a04c10c4f436566e58af9fb1dfdef4e
Denis Berekelya <cm-den@yandex.ru>
2023-01-14 01:37:53 +0300

    Создана автоматизация сборки проекта

--- gulpfile.js
+++ gulpfile.js
@@ -6,7 +6,11 @@ import autoprefixer from 'autoprefixer';
 import browser from 'browser-sync';
 import squoosh from 'gulp-libsquoosh';
 import svgSprite from 'gulp-svg-sprite';
-import { stacksvg } from "gulp-stacksvg";
+import { stacksvg } from 'gulp-stacksvg';
+import htmlmin from 'gulp-htmlmin';
+import svgmin from 'gulp-svgmin';
+import cssmin from 'gulp-cssmin';
+import {deleteAsync} from 'del';
 
 
 // Styles
@@ -71,11 +75,98 @@ export const createSvgStack = () => {
 
 export const createSvgSprite = () => {
   return gulp.src('source/img/decore/inline/*.svg')
-  .pipe (svgSprite({mode: {symbol: {sprite: 'decore-symbol-sprite.svg', dest: '.'}}, sgv: {dimensionAttributes: true}}))
+  .pipe (svgSprite({mode: {symbol: {sprite: 'decore-symbol-sprite.svg', dest: '.'}}}))
   .pipe (gulp.dest('source/img/decore'))
 }
 
+//HTML Minify
+
+export const htmlMin = () => {
+  return gulp.src('source/*.html')
+  .pipe(htmlmin({ collapseWhitespace: true }))
+  .pipe(gulp.dest('build'));
+}
+
+//IMGOptimize
+
+export const optImages = () => {
+  return gulp.src('source/img/**/*.{png,jpg}')
+  .pipe(squoosh())
+  .pipe(gulp.dest('build/img'))
+  }
+
+export  const svgOpt = () =>
+  gulp.src('source/img/*.svg')
+  .pipe(svgmin())
+  .pipe(gulp.dest('build/img'));
+
+//CSS Minify
+
+export const cssMin = (done) => {
+  gulp.src('source/css/style.css')
+  .pipe(cssmin())
+  .pipe(gulp.dest('build/css'))
+  done();
+}
+
+// Scripts
+
+export const scripts = () => {
+  return gulp.src(['source/js/*.js','!source/js/*--debug*'])
+  .pipe(gulp.dest('build/js'))
+  }
+
+//Copying fles
+
+export const copy = (done) => {
+  gulp.src('source/fonts/*.{woff,woff2}')
+  .pipe(gulp.dest('build/fonts/'))
+  gulp.src('source/img/**/*.webp')
+  .pipe(gulp.dest('build/img/'))
+  gulp.src('source/favicon.ico')
+  .pipe(gulp.dest('build/'))
+  gulp.src('source/manifest.webmanifest')
+  .pipe(gulp.dest('build/'))
+  gulp.src('source/img/favicons/*')
+  .pipe(gulp.dest('build/img/favicons/'))
+  gulp.src('source/img/decore/{decore-backgrounds-stack.svg,decore-symbol-sprite.svg}')
+  .pipe(gulp.dest('build/img/decore/'))
+  done();
+}
+
+// Clean
+
+export const clean = () => {
+  return deleteAsync('build')
+  };
+
+  //Build
+
+  export const build = gulp.series(
+    clean,
+    styles,
+    htmlMin,
+    optImages,
+    svgOpt,
+    cssMin,
+    scripts,
+    copy
+    );
 
 export default gulp.series(
   styles, server, watcher
 );
+
+// Server for build
+
+export const serverBuild = (done) => {
+  browser.init({
+    server: {
+      baseDir: 'build'
+    },
+    cors: true,
+    notify: false,
+    ui: false,
+  });
+  done();
+}
